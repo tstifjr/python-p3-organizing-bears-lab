b9649331cfd34018be850ea872dcd189d8d67596
TJ Stifter <tjstifter@gmail.com>
2023-07-12 15:40:39 -0400

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "Chocolate", "Friendly", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Black", "Rowdy", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Beige", "Quiet", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 16, "M", "Dark Brown", "Strict", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "Black", "Sassy", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "Green", "Greedy", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 11, "F", "Red", "Hungry", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL)
